96a7d80fd9b14803fdd9e5991097db756da9e042
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2022-08-28 23:47:35 -0300

    teste

---
+++ aula_12/LeituraDadosTeclado.java
@@ -0,0 +1,15 @@
+package aula_12;
+
+import java.util.Scanner;
+
+public class LeituraDadosTeclado{
+
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite seu nome completo:");
+        String nomeCompleto = scan.nextLine();
+    }
+
+    public LeituraDadosTeclado() {
+    }
+}
