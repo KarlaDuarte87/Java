6b44f6a6e06f960ff87fa4c0bf3fb46649d17595
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2022-09-01 01:36:20 -0300

    aula 13 exe 1

---
+++ aula_13/exe3.java
@@ -0,0 +1,17 @@
+package aula_13;
+
+public class exe3 {
+    public static void main (Stringa[] args){
+        Scanner scan = new Scanner (System.in);
+
+        System.out .println ("Digite um valor de entrada: ");
+        int num1 = scan.nextInt();
+
+        System.out .println("Digite o segumdo valor de entrada: ");
+        int num2 = scan.nextInt();
+
+        int resultado = num1 + num2;
+        System.our.println ("A soma dos numeros é: "+ resultado);
+    }
+    
+}
